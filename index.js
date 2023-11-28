0ec31932dbdfae1682f912a7010aa3bc5d418526
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2023-11-28 02:05:10 -0500

    lab completed

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b
+    console.log(1000, 329)
+}
+
+function subtract(a, b) {
+    return a - b
+    console.log(1, 355)
+}
+function multiply(a, b) {
+    return a * b
+    console.log(4545, 15)
+}
+function divide(a, b) {
+    return a / b
+    console.log(1.1367041198501873, 1)
+}
+
+function increment(n) {
+    return ++n
+    console.log(965)
+}
+
+function decrement(n) {
+    return --n
+    console.log(965)
+}
+
+function makeInt(n) {
+    return parseInt(n, 10)
+    console.log(232.56)
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n)
+    console.log(53.6589)
+}
